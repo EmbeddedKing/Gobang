c757f6110ceb3e5d2270a7f30792fa73397b51a2
舒艺彬 <1321598575@qq.com>
2018-03-22 13:47:56 +0800

    2018.3.22
    /****************************************
     * 1.添加注释
     * 2.修改部分Bug
     ***************************************/

--- Gobang.c
+++ Gobang.c
@@ -2,46 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 17
-#define CONDITION 5
+#define MAX 17		//棋盘大小，15*15有两行数字标识
+#define CONDITION 5		//胜利条件：五子连珠
 
-void CheckerBoard_Init();
-void CheckerBoard_Display();
-int CheckerBoard_Play();
-int White_Play(int x,int y);
-int Black_Play(int x,int y);
+void CheckerBoard_Init();			//棋盘初始化函数
+void CheckerBoard_Display();			//棋盘显示函数
+int CheckerBoard_Play();				//对局开始函数
+int White_Play(int x,int y);			//白子落子期间
+int Black_Play(int x,int y);			//黑子落子期间
 
 typedef struct{
 	int x;
 	int y;
-}coordinate;
+}coordinate;		//坐标结构体
 
-char CheckerBoard[MAX][MAX];
-coordinate position;
-int Step_Number;
-int Victory_Flag;
+char CheckerBoard[MAX][MAX];		//地图二维数组
+coordinate position;				//用来接收用户输入的坐标
+int Step_Number;					//用来计算步数
+int Victory_Flag;					//胜利结果
 
 int main()
 {
-	CheckerBoard_Init();
-	CheckerBoard_Display();
-	while(!Victory_Flag)
+	CheckerBoard_Init();		//棋盘初始化
+	CheckerBoard_Display();		//棋盘显示
+	while(!Victory_Flag)		//当有结果时跳出循环
 	{
-		Victory_Flag=CheckerBoard_Play();
+		Victory_Flag=CheckerBoard_Play();	//用来接收对局结果
 	}
 	switch(Victory_Flag)
 	{
-		case 1:
+		case 1:			//如果接收为1，则白棋胜利
 			printf("=======================================\n");
 			printf("		  White Win! Steps:%d\n",Step_Number);
 			printf("=======================================\n");
 			break;
-		case 2:
+		case 2:			//如果接收为2，则黑旗胜利
 			printf("=======================================\n");
 			printf("          Black Win! Steps:%d\n",Step_Number);
 			printf("=======================================\n");
 			break;
-		case 3:
+		case 3:			//如果接收为3，则平局
 			printf("=======================================\n");
 			printf("              Draw! Steps:%d\n",Step_Number);
 			printf("=======================================\n");
@@ -49,14 +49,14 @@ int main()
 	return 0;
 }
 
-void CheckerBoard_Init()
+void CheckerBoard_Init()		//棋盘初始化函数
 {
 	int i,j;
 	for(i=0;i<MAX;i++)
 	{
 		for(j=0;j<MAX;j++)
 		{
-			if(i==0||i==MAX-1)
+			if(i==0||i==MAX-1)			//如果是棋盘边缘则赋给他对应坐标
 			{
 				CheckerBoard[i][j]=j;
 			}
@@ -64,7 +64,7 @@ void CheckerBoard_Init()
 			{
 				CheckerBoard[i][j]=i;
 			}
-			else
+			else						//如果不是棋盘边缘则赋给他'.'
 			{
 				CheckerBoard[i][j]='.';
 			}
@@ -72,14 +72,14 @@ void CheckerBoard_Init()
 	}
 }
 
-void CheckerBoard_Display()
+void CheckerBoard_Display()				//显示棋盘函数
 {
 	int i,j;
 	for(i=0;i<MAX;i++)
 	{
 		for(j=0;j<MAX;j++)
 		{
-			if(i==0||i==MAX-1)
+			if(i==0||i==MAX-1)			//如果是棋盘边缘则用打印数字
 			{
 				printf("%2d",CheckerBoard[i][j]);
 			}
@@ -87,7 +87,7 @@ void CheckerBoard_Display()
 			{
 				printf("%2d",CheckerBoard[i][j]);
 			}
-			else
+			else				//如果不是棋盘边缘则打印字符
 			{
 				printf(" %c",CheckerBoard[i][j]);
 			}
@@ -96,12 +96,12 @@ void CheckerBoard_Display()
 	}
 }
 
-int CheckerBoard_Play()
+int CheckerBoard_Play()			//对局期间函数
 {
 	int Flag=3;
 	while(Flag==3 && Step_Number!=(MAX-2)*(MAX-2))
 	{
-		if(!(Step_Number%2))
+		if(!(Step_Number%2))		//如果步数为单数则黑旗落子阶段，如果为双数则白棋落子阶段
 		{
 			printf("=======================================\n");
 			printf("    Black:x    Steps:%d\n",Step_Number+1);
@@ -114,85 +114,85 @@ int CheckerBoard_Play()
 			printf("=======================================\n");
 		}
 		printf("input position x and y:");
-		scanf("%d%d",&position.x,&position.y);
-		scanf("%*[^\n]");
+		scanf("%d%d",&position.x,&position.y);		//接收用户的坐标
+		scanf("%*[^\n]");			//清空输入缓存区
 		scanf("%*c");
-		if(position.x==0||position.x>=MAX-1||position.y==0||position.y>=MAX-1)
+		if(position.x==0||position.x>=MAX-1||position.y==0||position.y>=MAX-1)	//如果落子落在棋盘外则提示超出边界
 		{
 			printf("=======================================\n");
 			printf("         Beyond the Boundary!\n");
 			printf("=======================================\n");
 		}
-		else if(CheckerBoard[position.x][position.y]=='.')
+		else if(CheckerBoard[position.x][position.y]=='.')			//如果没有落在棋盘外并且此位置没有被别的棋子占领就执行落子程序
 		{
 			if(!(Step_Number % 2))
 			{
-				Flag=Black_Play(position.x,position.y);
+				Flag=Black_Play(position.x,position.y);		//步数为单执行黑子落子程序
 			}
 			else
 			{
-				Flag=White_Play(position.x,position.y);
+				Flag=White_Play(position.x,position.y);		//步数为双执行白子落子程序
 			}
-			Step_Number++;
+			Step_Number++;		//步数加一
 		}
-		else
+		else			//如果落子位置已经存在别的棋子，则提示覆盖错误
 		{
 			printf("=======================================\n");
 			printf("                Cover!\n");
 			printf("=======================================\n");
 		}
-		CheckerBoard_Display();
+		CheckerBoard_Display();	//打印棋盘
 	}
 	return Flag;
 }
 
-int White_Play(int x,int y)
+int White_Play(int x,int y)		//白棋落子程序
 {
-	int i,Transverse=1,Vertical=1,LeftOblique=1,RightOblique=1;
-	CheckerBoard[x][y]='o';
-	for(i=1;CheckerBoard[x-i][y]=='o';i++)
+	int i,Transverse=1,Vertical=1,LeftOblique=1,RightOblique=1;	//三个变量用来计算横竖，左斜右斜的连珠数量，如果达到5颗则白棋判胜
+	CheckerBoard[x][y]='o';						//把用户输入的落子位置改成'o'
+	for(i=1;CheckerBoard[x-i][y]=='o';i++)		//计算落子位置竖向上边连珠的个数
 	{
 		Vertical++;
 	}
-	for(i=1;CheckerBoard[x+i][y]=='o';i++)
+	for(i=1;CheckerBoard[x+i][y]=='o';i++)		//计算落子位置竖向下边连珠的个数
 	{
 		Vertical++;
 	}
-	for(i=1;CheckerBoard[x][y-i]=='o';i++)
+	for(i=1;CheckerBoard[x][y-i]=='o';i++)		//计算落子位置横向左边连珠的个数
 	{
 		Transverse++;
 	}
-	for(i=1;CheckerBoard[x][y+i]=='o';i++)
+	for(i=1;CheckerBoard[x][y+i]=='o';i++)		//计算落子位置横向右边连珠的个数
 	{
 		Transverse++;
 	}
-	for(i=1;CheckerBoard[x-i][y+i]=='o';i++)
+	for(i=1;CheckerBoard[x-i][y+i]=='o';i++)	//计算落子位置左斜向右上边连珠的个数
 	{
 		LeftOblique++;
 	}
-	for(i=1;CheckerBoard[x+i][y-i]=='o';i++)
+	for(i=1;CheckerBoard[x+i][y-i]=='o';i++)	//计算落子位置左斜向左下边连珠的个数
 	{
 		LeftOblique++;
 	}
-	for(i=1;CheckerBoard[x+i][y+i]=='o';i++)
+	for(i=1;CheckerBoard[x+i][y+i]=='o';i++)	//计算落子位置右斜向右下边连珠的个数
 	{
 		RightOblique++;
 	}
-	for(i=1;CheckerBoard[x-i][y-i]=='o';i++)
+	for(i=1;CheckerBoard[x-i][y-i]=='o';i++)	//计算落子位置右斜向左上边连珠的个数
 	{
 		RightOblique++;
 	}
 	if( Vertical==CONDITION || 
 		Transverse==CONDITION ||
 		LeftOblique==CONDITION ||
-		RightOblique==CONDITION)
+		RightOblique==CONDITION)		//如果连珠的个数达到判胜条件则返回1
 	{
-		return 2;
+		return 1;
 	}
-	return 0;
+	return 3;		//如果没有达到则返回3，继续循环
 }
 
-int Black_Play(int x,int y)
+int Black_Play(int x,int y)		//黑子落子阶段，代码和白棋一样，黑旗达到胜利条件返回2
 {
 	int i,Transverse=1,Vertical=1,LeftOblique=1,RightOblique=1;
 	CheckerBoard[x][y]='x';
@@ -235,5 +235,5 @@ int Black_Play(int x,int y)
 	{
 		return 2;
 	}
-	return 0;
+	return 3;
 }
